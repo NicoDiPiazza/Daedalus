46b1755b97b82f52c30778ec13ae4e4b3e0c31b3
Paul Zupan <paul.zupan.13@gmail.com>
2022-09-24 20:48:09 -0700

    Add comments and simplify generation logic

--- dungeon.py
+++ dungeon.py
@@ -1,9 +1,10 @@
 import random
+from typing import Union
 
 # variables for generation
 
 
-def generate(rooms: int, boss: bool, shop: bool):
+def generate(rooms: int, boss: bool, shop: bool) -> list[list[Union[int, str]]]:
     roomsNeeded = 2
     if boss:
         roomsNeeded = roomsNeeded + 1
@@ -14,17 +15,21 @@ def generate(rooms: int, boss: bool, shop: bool):
         print("Not enough rooms!")
         return "Not enough rooms!"
 
-    dunArray = []
-    startGridCoords = [5, 5]
+    dunArray: list[list[Union[int, str]]] = [[5, 5]]
 
-    # entry = floor(random() * (N_rooms - 1))
+    # pick a random room number to be the entry room
     entry = random.randrange(rooms - 1)
+
+    # pick a room for the boss, but make sure it's not the entry room
     newBoss = boss
     bossRoom = -1
     while newBoss:
         # bossRoom = floor(random_func() * (N_rooms - 1))
         bossRoom = random.randrange(rooms - 1)
         newBoss = bossRoom == entry
+
+    # pick a room number for the shop, but make sure it's not one of the rooms
+    # for boss or entry
     newShop = shop
     shopRoom = -1
     while newShop:
@@ -33,32 +38,22 @@ def generate(rooms: int, boss: bool, shop: bool):
         newShop = shopRoom == entry or shopRoom == bossRoom
 
     # coordinate generation
-    for i in range(rooms):
-
-        if i > 0:
-            redo = True
-
-            while redo:
-                direction = random.random() * 4
-                if direction <= 1:
-                    newCoords = [dunArray[i - 1][0] - 1, dunArray[i - 1][1]]
-                elif direction <= 2:
-                    newCoords = [dunArray[i - 1][0] + 1, dunArray[i - 1][1]]
-                elif direction <= 3:
-                    newCoords = [dunArray[i - 1][0], dunArray[i - 1][1] - 1]
-                else:
-                    newCoords = [dunArray[i - 1][0], dunArray[i - 1][1] + 1]
-
-                redo = newCoords in dunArray
-
-            # newCoords: Unbound | list[Unknown]
-            dunArray.append(newCoords)
-        else:
-            # startGridCoords: list[int]
-            dunArray.append(startGridCoords)
+    for i in range(rooms - 1):
+        redo = True
+        while redo:
+            direction = random.random() * 4
+            if direction <= 1:
+                newCoords = [dunArray[i - 1][0] - 1, dunArray[i - 1][1]]
+            elif direction <= 2:
+                newCoords = [dunArray[i - 1][0] + 1, dunArray[i - 1][1]]
+            elif direction <= 3:
+                newCoords = [dunArray[i - 1][0], dunArray[i - 1][1] - 1]
+            else:
+                newCoords = [dunArray[i - 1][0], dunArray[i - 1][1] + 1]
+            redo = newCoords in dunArray
+        dunArray.append(newCoords)
 
     for i in range(rooms):
-
         if i == entry:
             dunArray[i].append("entry")
         elif i == rooms - 1:
@@ -67,7 +62,6 @@ def generate(rooms: int, boss: bool, shop: bool):
             dunArray[i].append("boss")
         elif i == shopRoom and shop:
             dunArray[i].append("shop")
-
         else:
             dunArray[i].append("basic")
 
